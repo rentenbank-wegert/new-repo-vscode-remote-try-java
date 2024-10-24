0d4ccf3bb7c9eb713cc03227ecabc1a1462853c0
rentenbank-wegert <bernd.wegert@rentenbank.de>
2024-10-24 08:18:04 +0000

    first commit

---
+++ src/main/java/com/mycompany/app/App2.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+/*----------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *---------------------------------------------------------------------------------------*/
+
+
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Remote World!");
+    }
+}
