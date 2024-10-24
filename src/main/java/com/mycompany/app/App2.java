e805b84564d5e8e591624d65612983f1a5a98bd7
rentenbank-wegert <bernd.wegert@rentenbank.de>
2024-10-24 13:25:31 +0000

    output change

--- src/main/java/com/mycompany/app/App2.java
+++ src/main/java/com/mycompany/app/App2.java
@@ -8,6 +8,6 @@ package com.mycompany.app;
 
 public class App2 {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        System.out.println("Hello Remote World2!");
     }
 }
